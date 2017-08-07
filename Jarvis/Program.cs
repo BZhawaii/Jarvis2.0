63cb6bc9e3083ac03afa668a159e347b0bc76128
Richard Beese <bzhawaii@yahoo.com>
2017-08-07 17:50:25 -0600

    Initial commit

--- Jarvis/Program.cs
+++ Jarvis/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Jarvis
 {
     class Program
     {
+        /// <summary>
+        /// Where all the magic happens
+        /// </summary>
+        /// <param name="args"></param>
+
         static void Main(string[] args)
         {
+            // This will pull the current CPU load in percentage
+            PerformanceCounter perfCpuCount = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+
+            // Infinite while loop
+            while(true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("CPU Load: {0}", perfCpuCount.NextValue());
+            }
         }
     }
 }
